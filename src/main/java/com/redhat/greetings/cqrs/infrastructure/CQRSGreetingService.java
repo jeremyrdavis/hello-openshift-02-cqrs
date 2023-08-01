1701fb041621ab08248cd0aabe1fbfad117ebbe1
jeremyrdavis <jeremyrdavis@mac.com>
2023-08-01 00:19:00 -0400

    persisting new greetings from kafka

--- src/main/java/com/redhat/greetings/cqrs/infrastructure/CQRSGreetingService.java
+++ src/main/java/com/redhat/greetings/cqrs/infrastructure/CQRSGreetingService.java
@@ -1,9 +1,12 @@
 package com.redhat.greetings.cqrs.infrastructure;
 
+import com.redhat.greetings.cqrs.domain.Greeting;
 import com.redhat.greetings.cqrs.domain.GreetingDTO;
 import com.redhat.greetings.cqrs.domain.GreetingRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +20,16 @@ public class CQRSGreetingService {
 
     @Inject
     GreetingRepository greetingRepository;
+
+    @Incoming("greetings-verified")
+    @Transactional
+    public void onVerifiedGreeting(GreetingDTO greetingDTO) {
+
+        Greeting greeting = new Greeting(greetingDTO.text(), greetingDTO.author(), greetingDTO.sourceSystem(), greetingDTO.createdAt(), greetingDTO.isVerifiedFamilyFriendly());
+        greeting.persist();
+        LOGGER.debug("persisted: {}", greeting);
+    }
+
 
     public List<GreetingDTO> listAllGreetings() {
 
